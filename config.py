8a40c381391bab730d0b7a3a50f43dc7d19ee74d
mikedws <131862913+mikedws@users.noreply.github.com>
2023-05-31 00:20:04 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "5663968047:AAGjt4kMXdaM66x8V1DU07Mjxc4wD5ResiI"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,37 @@
+from builtins import Exception, staticmethod, KeyError, ValueError, float, str
+
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class Converter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'⛔ Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f' Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f' Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f' Не удалось обработать количество {amount}')
+
+        quote_ticker, base_ticker = keys[quote], keys[base]
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
